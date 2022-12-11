abb0c1f47938d7f3d06ffb634609017c514ecf10
patrickcberry <patrickcberry@gmail.com>
2022-12-11 21:58:31 +1100

    Update

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ then
     echo "Folder already exists"
 else
     echo "Creating folder guacamole-config"
-    md guacamole-config    
+    mkdir guacamole-config    
 fi
 
 ### docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --mysql > initdb.sql
