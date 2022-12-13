a17e34e80456f697259a1d16fc9fbebbe3c17f85
patrickcberry <patrickcberry@gmail.com>
2022-12-13 21:47:58 +1100

    Updated

--- setup.sh
+++ setup.sh
@@ -8,6 +8,20 @@ else
     mkdir guacamole-config    
 fi
 
+# Check is Docker is installed
+if [ -x "$(command -v docker)" ]; then
+    echo "Update docker"
+    # command
+else
+    echo "Install docker"
+    # command
+fi
+
+# Download required Docker images
+# docker pull guacamole/guacamole 
+# docker pull guacamole/guacd 
+# docker pull mysql/mysql-server
+
 # Run the guacamole client to generate a database configuration
 # script for mysql
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --mysql > ./guacamole-config/initdb.sql
